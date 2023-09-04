7392e5f0462b9853ef9a89d512a5ec86e2ec929d
Thales Lima <thales@inovaebiz.com.br>
2023-09-03 22:39:41 -0300

    Implementação do primeiro endpoint (Login)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
+app.use(express.json());
+const BaseURL = "https://data.mongodb-api.com/app/application-0-mqvuy/endpoint/";
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.status(200).send("API running");
 });
 
-app.get("/Login", (req, res) =>{
-    res.status(200).json({ msg: 'Bem Vindo' });
+app.post("/Login", async (req, res) => {
+    try {
+        const mongoReq = await axios.post(`${BaseURL}Login`, { apelido: req.body.apelido, senha: req.body.senha })
+            .then((mongoRes) => {
+                res.status(200).json({ Token: mongoRes.data.Token, Apelido: mongoRes.data.Apelido, Email: mongoRes.data.Email });
+            })
+            .catch((mongoError) => {
+                console.log(mongoError.response.data);
+                res.status(mongoError.response.status).json({ Erro: mongoError.response.data.error });
+            });
+    }
+    catch (e) {
+        res.status(500).json({ Erro: `Ocorreu uma falha no processamento da API: ${e.message}` });
+    }
 });
 
 export default app;
