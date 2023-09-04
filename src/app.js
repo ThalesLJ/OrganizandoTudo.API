5d5acfe6954be77f9e999ec94e67b93625122301
Thales Lima <thales@inovaebiz.com.br>
2023-09-04 20:44:54 -0300

    Implementação dos endpoints dos Usuários

--- src/app.js
+++ src/app.js
@@ -6,22 +6,88 @@ app.use(express.json());
 const BaseURL = "https://data.mongodb-api.com/app/application-0-mqvuy/endpoint/";
 
 app.get("/", (req, res) => {
-    res.status(200).send("API running");
+    try {
+        res.status(200).json({ status: "OK" });
+    } catch (e) {
+        res.status(500).json({ status: "ERROR" });
+    }
+});
+
+app.post("/CreateAccount", async (req, res) => {
+    try {
+        if (req.body.name == null || req.body.email == null || req.body.password == null) {
+            res.status(500).json({ message: "Preencha todos os campos!", code: "Algum dos dados informados está vazio" });
+        } else {
+            const mongoReq = await axios.post(`${BaseURL}CriarConta`, { apelido: req.body.name, email: req.body.email, senha: req.body.password })
+                .then((mongoRes) => {
+                    res.status(mongoRes.status).json({ message: mongoRes.data.message, code: mongoRes.data.code });
+                })
+                .catch((mongoError) => {
+                    res.status(mongoError.response.status).json({ message: mongoError.response.data.message, code: mongoError.response.data.code });
+                });
+        }
+    } catch (e) {
+        res.status(500).json({ Error: e.message });
+    }
 });
 
 app.post("/Login", async (req, res) => {
     try {
-        const mongoReq = await axios.post(`${BaseURL}Login`, { apelido: req.body.apelido, senha: req.body.senha })
+        if (req.body.name == null || req.body.password == null) {
+            res.status(500).json({ message: "Preencha todos os campos!", error_code: "Algum dos dados informados está vazio" });
+        } else {
+            const mongoReq = await axios.post(`${BaseURL}Login`, { apelido: req.body.name, senha: req.body.password })
+                .then((mongoRes) => {
+                    res.status(mongoRes.status).json({ token: mongoRes.data.Token, name: mongoRes.data.Apelido, email: mongoRes.data.Email });
+                })
+                .catch((mongoError) => {
+                    res.status(mongoError.response.status).json({ message: mongoError.response.data.error });
+                });
+        }
+    } catch (e) {
+        res.status(500).json({ Error: e.message });
+    }
+});
+
+app.post("/VerifyToken", async (req, res) => {
+    try {
+        const mongoReq = await axios.post(`${BaseURL}ValidarToken`, {}, { headers: { 'Authorization': req.headers.authorization } })
             .then((mongoRes) => {
-                res.status(200).json({ Token: mongoRes.data.Token, Apelido: mongoRes.data.Apelido, Email: mongoRes.data.Email });
+                res.status(mongoRes.status).json({ message: mongoRes.data.message, code: mongoRes.data.code });
             })
             .catch((mongoError) => {
-                console.log(mongoError.response.data);
-                res.status(mongoError.response.status).json({ Erro: mongoError.response.data.error });
+                res.status(mongoError.response.status).json({ message: mongoError.response.data.message, code: mongoError.response.data.code });
             });
+    } catch (e) {
+        res.status(500).json({ Error: e.message });
     }
-    catch (e) {
-        res.status(500).json({ Erro: `Ocorreu uma falha no processamento da API: ${e.message}` });
+});
+
+app.get("/User", async (req, res) => {
+    try {
+        const mongoReq = await axios.get(`${BaseURL}Perfil`, { headers: { 'Authorization': req.headers.authorization } })
+            .then((mongoRes) => {
+                res.status(mongoRes.status).json({ name: mongoRes.data.apelido, email: mongoRes.data.email });
+            })
+            .catch((mongoError) => {
+                res.status(mongoError.response.status).json({ message: mongoError.response.data.message, code: mongoError.response.data.code });
+            });
+    } catch (e) {
+        res.status(500).json({ Error: e.message });
+    }
+});
+
+app.put("/User", async (req, res) => {
+    try {
+        const mongoReq = await axios.put(`${BaseURL}Perfil`, { dados: { apelido: req.body.name, email: req.body.email, senha: req.body.password } }, { headers: { 'Authorization': req.headers.authorization } })
+            .then((mongoRes) => {
+                res.status(mongoRes.status).json({ message: mongoRes.data.message, code: mongoRes.data.code });
+            })
+            .catch((mongoError) => {
+                res.status(mongoError.response.status).json({ message: mongoError.response.data.message, code: mongoError.response.data.code });
+            });
+    } catch (e) {
+        res.status(500).json({ Error: e.message });
     }
 });
 
