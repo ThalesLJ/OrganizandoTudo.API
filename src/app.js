edfcde4d6bb69e076741df2d9e1304afa41b823f
Thales Lima <thales@inovaebiz.com.br>
2023-09-04 21:58:31 -0300

    Difinição de tamanho minimo dos campos para Criar Conta

--- src/app.js
+++ src/app.js
@@ -18,13 +18,19 @@ app.post("/CreateAccount", async (req, res) => {
         if (req.body.name == null || req.body.email == null || req.body.password == null) {
             res.status(500).json({ message: "Preencha todos os campos!", code: "Algum dos dados informados está vazio" });
         } else {
-            const mongoReq = await axios.post(`${BaseURL}CriarConta`, { apelido: req.body.name, email: req.body.email, senha: req.body.password })
-                .then((mongoRes) => {
-                    res.status(mongoRes.status).json({ message: mongoRes.data.message, code: mongoRes.data.code });
-                })
-                .catch((mongoError) => {
-                    res.status(mongoError.response.status).json({ message: mongoError.response.data.message, code: mongoError.response.data.code });
-                });
+            if (req.body.name.toString().length < 3) {
+                res.status(500).json({ message: "Preencha os campos corretamente!", code: "O apelido deve ter no minimo 03 caracteres" });
+            } else if (req.body.password.toString().length < 5) {
+                res.status(500).json({ message: "Preencha os campos corretamente!", code: "A senha deve ter no minimo 05 caracteres" });
+            } else {
+                const mongoReq = await axios.post(`${BaseURL}CriarConta`, { apelido: req.body.name, email: req.body.email, senha: req.body.password })
+                    .then((mongoRes) => {
+                        res.status(mongoRes.status).json({ message: mongoRes.data.message, code: mongoRes.data.code });
+                    })
+                    .catch((mongoError) => {
+                        res.status(mongoError.response.status).json({ message: mongoError.response.data.message, code: mongoError.response.data.code });
+                    });
+            }
         }
     } catch (e) {
         res.status(500).json({ Error: e.message });
